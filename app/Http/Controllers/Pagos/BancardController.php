948b248f89a9f5876c0430f63d835afd2004010f
alexvazu8 <alexvazu@gmail.com>
2025-07-02 12:11:37 -0400

    cambios en el callback

--- app/Http/Controllers/Pagos/BancardController.php
+++ app/Http/Controllers/Pagos/BancardController.php
@@ -153,7 +153,7 @@ class BancardController extends Controller
       // Datos que Laravel interpreta (GET y POST form)
       $data = $request->all();
       print_r($data);
-      if(isset($data['id_pago']))
+      if(isset($data['id_pago'])&& $data['status']=="payment_success")
       {
         echo $data['id_pago'];
       }
