8c0b155cd6236f02c4ea89e5e38bc98cab4a2cac
alexvazu8 <alexvazu@gmail.com>
2025-07-11 09:00:19 -0400

    Arreglo alert en error de pago Bancard

--- resources/views/carritos/show.blade.php
+++ resources/views/carritos/show.blade.php
@@ -464,6 +464,7 @@
                 }, 700);
         }//fin del si el pago es satisfactorio
         else if (status === "payment_fail") {
+            alert("hola");
             // Verificación de estado para pago fallido
             const checkInterval = setInterval(async () => {
                 const statusResponse = await fetch(`/pagos/verificar-estado`);
