04a11f52eb1846858c393137c4b3e1454062424b
alexvazu8 <alexvazu@gmail.com>
2025-09-19 11:10:05 -0400

    pagos

--- app/Http/Controllers/Api/PagoController.php
+++ app/Http/Controllers/Api/PagoController.php
@@ -267,7 +267,7 @@ class PagoController extends Controller
         
         $pago = Pago::where('usuario_id', $usuarioId)
                     ->where('transaction_id_metodo_pago', $transactionId)
-                   ->where('expiration_token', '>', $now)
+                   // ->where('expiration_token', '>', $now)
                     ->where('token', $tokenVM)
                     ->latest()
                     ->first();
