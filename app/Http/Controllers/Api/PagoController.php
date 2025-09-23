d4af15f80e65a7e099cc02b3b9653dbc09b694c8
alexvazu8 <alexvazu@gmail.com>
2025-09-23 10:59:25 -0400

    usando solo usuario

--- app/Http/Controllers/Api/PagoController.php
+++ app/Http/Controllers/Api/PagoController.php
@@ -264,7 +264,7 @@ class PagoController extends Controller
          $transactionId = $request->query('transaction_id');
         $tokenVM = $request->query('token');
         $now = Carbon::now()->timestamp;
-        $pago = Pago::where('usuario_id', $usuarioId);
+        $pago = Pago::where('usuario_id', $usuarioId)->latest()->first();
         print_r($pago);
        // echo $pago->estado;
        /* $pago = Pago::where('usuario_id', $usuarioId)
