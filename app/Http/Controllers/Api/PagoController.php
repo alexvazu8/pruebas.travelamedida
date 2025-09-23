e1f7e18432ae3a6ce43a6e707c29b534ef785aaa
alexvazu8 <alexvazu@gmail.com>
2025-09-23 11:17:34 -0400

    cambios pago

--- app/Http/Controllers/Api/PagoController.php
+++ app/Http/Controllers/Api/PagoController.php
@@ -266,7 +266,7 @@ class PagoController extends Controller
         $now = Carbon::now()->timestamp;
         $pago = Pago::where('usuario_id', $usuarioId)->where('transaction_id_metodo_pago', $transactionId)->where('token', $tokenVM);
         echo $pago->toSql();
-        //print_r($pago);
+        print_r($pago);
        // echo $pago->estado;
        /* $pago = Pago::where('usuario_id', $usuarioId)
                     ->where('transaction_id_metodo_pago', $transactionId)
