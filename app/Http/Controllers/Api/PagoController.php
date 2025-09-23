ae53e21feae5872dc95f38af1f28f2e37acf41a4
alexvazu8 <alexvazu@gmail.com>
2025-09-23 10:57:30 -0400

    pago2!

--- app/Http/Controllers/Api/PagoController.php
+++ app/Http/Controllers/Api/PagoController.php
@@ -264,7 +264,7 @@ class PagoController extends Controller
          $transactionId = $request->query('transaction_id');
         $tokenVM = $request->query('token');
         $now = Carbon::now()->timestamp;
-        $pago = Pago::where('usuario_id', $usuarioId)->where('transaction_id_metodo_pago', $transactionId)->where('expiration_token', '>', $now)->where('token', $tokenVM)->latest()->first();
+        $pago = Pago::where('usuario_id', $usuarioId)->latest()->first();
         print_r($pago);
        // echo $pago->estado;
        /* $pago = Pago::where('usuario_id', $usuarioId)
