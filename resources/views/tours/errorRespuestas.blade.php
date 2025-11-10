284befb089170bae45428874df9f9f0045ca249d
alexvazu8 <alexvazu@gmail.com>
2025-11-10 17:03:36 -0400

    Pruebas error tours

---
+++ resources/views/tours/errorRespuestas.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Respuestas Tours</h1>
+    @php
+    //print_r($respuestas);
+    @endphp
+    @if(empty($respuestas))
+    <p>No hay respuestas para mostrar</p>
+    <a href="{{ url('/tours') }}" class="btn btn-primary btn-lg mx-2">Tours</a>
+    @else
+        @if(isset($respuestas['error']))
+            {{$respuestas['error']}}
+            @if(isset($respuestas['validation_errors']))
+                @foreach ($respuestas['validation_errors'] as $error)
+                    @foreach ($error as $detelle_error)
+                        <p>{{ $detelle_error }}</p>
+                    @endforeach
+                @endforeach
+            @endif
+            <a href="{{ url('/tours') }}" class="btn btn-primary btn-lg mx-2">Tours</a>
+        @else
+            <p> <strong>Error no  detectado.</strong> </p>
+            <a href="{{ url('/tours') }}" class="btn btn-primary btn-lg mx-2">Tours</a>
+        @endif
+    @endif
