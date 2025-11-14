844c34f53d37dae882890c556a0e0252801447f7
alexvazu8 <alexvazu@gmail.com>
2025-11-14 12:10:38 -0400

    cambios welcome cama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                 <a href="{{ url('/hoteles') }}" class="btn btn-secondary btn-hero">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                            d="M3 7v10m0 0h18M3 17v-4a2 2 0 012-2h4a2 2 0 012 2v4M7 11V7a2 2 0 012-2h6a2 2 0 012 2v4m4 0h2a2 2 0 012 2v4h-4v-4a2 2 0 00-2-2h-2"/>
+                            d="M3 7v10h18V7M3 17v-4a2 2 0 012-2h4a2 2 0 012 2v4M7 11V7a2 2 0 012-2h6a2 2 0 012 2v4"/>
                     </svg>
                     {{ __('Hoteles') }}
                 </a>
