5cc7700fa047e3f11eccf59252df144a82ce7a52
alexvazu8 <alexvazu@gmail.com>
2026-01-05 08:01:08 -0400

    Voucher traducciones

--- resources/views/reservas/detalles/tour.blade.php
+++ resources/views/reservas/detalles/tour.blade.php
@@ -6,21 +6,21 @@
     <div class="card-body">
         <div class="row">
             <div class="col-md-6">
-                <p><strong>Nombre del Tour:</strong> {{ $data['detalle_tour']['tour']['Nombre_tour'] }}</p>
-                <p><strong>Email de Contacto:</strong> {{ $data['detalle_tour']['tour']['Email_contacto_tour'] }}</p>
-                <p><strong>Duración:</strong> {{ $data['detalle_tour']['tour']['cantidad_dias_tour'] }} días / {{ $data['detalle_tour']['tour']['cantidad_noches_tour'] }} noches</p>
-                <p><strong>Punto de Encuentro:</strong> {{ $data['detalle_tour']['tour']['Punto_encuentro'] }}</p>
+                <p><strong>{{__("Tours")}}:</strong> {{ $data['detalle_tour']['tour']['Nombre_tour'] }}</p>
+                <p><strong>{{__("Email_contacto")}}:</strong> {{ $data['detalle_tour']['tour']['Email_contacto_tour'] }}</p>
+                <p><strong>{{__("Duracion")}}:</strong> {{ $data['detalle_tour']['tour']['cantidad_dias_tour'] }} días / {{ $data['detalle_tour']['tour']['cantidad_noches_tour'] }} noches</p>
+                <p><strong>{{__("Punto_encuentro")}}:</strong> {{ $data['detalle_tour']['tour']['Punto_encuentro'] }}</p>
             </div>
             <div class="col-md-6">
-                <p><strong>Fecha Ingreso:</strong> {{ \Carbon\Carbon::parse($data['detalle_tour']['Fecha_In'])->format('d/m/Y') }}</p>
-                <p><strong>Fecha Salida:</strong> {{ \Carbon\Carbon::parse($data['detalle_tour']['Fecha_Out'])->format('d/m/Y') }}</p>
-                <p><strong>Precio Adulto:</strong> ${{ number_format($data['detalle_tour']['Precio_Adulto'], 2) }}</p>
-                <p><strong>Precio Menor:</strong> ${{ number_format($data['detalle_tour']['Precio_Menor'], 2) }}</p>
-                <p><strong>Precio Total:</strong> ${{ number_format($data['detalle_tour']['Precio_Total'], 2) }}</p>
+                <p><strong>{{__("Fecha_desde")}}:</strong> {{ \Carbon\Carbon::parse($data['detalle_tour']['Fecha_In'])->format('d/m/Y') }}</p>
+                <p><strong>{{__("Fecha_hasta")}}:</strong> {{ \Carbon\Carbon::parse($data['detalle_tour']['Fecha_Out'])->format('d/m/Y') }}</p>
+                <p><strong>{{__("Price_adulto")}}:</strong> ${{ number_format($data['detalle_tour']['Precio_Adulto'], 2) }}</p>
+                <p><strong>{{__("Price_menor")}}:</strong> ${{ number_format($data['detalle_tour']['Precio_Menor'], 2) }}</p>
+                <p><strong>{{__("Price_total")}}:</strong> ${{ number_format($data['detalle_tour']['Precio_Total'], 2) }}</p>
             </div>
         </div>
         <hr>
-        <h5>Descripción del Tour:</h5>
+        <h5>{{__("Descripcion")}}:</h5>
         <div>{!! $data['detalle_tour']['tour']['Detalle_tour'] !!}</div>
     </div>
 </div>
