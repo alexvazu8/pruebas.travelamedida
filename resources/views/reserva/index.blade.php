0982a29cc15a2289798a8104e003ef5d2c22526e
alexvazu8 <alexvazu@gmail.com>
2026-04-24 13:42:52 -0400

    index admin

--- resources/views/reserva/index.blade.php
+++ resources/views/reserva/index.blade.php
@@ -57,7 +57,7 @@
 										<td >{{ $reserva->Comentarios }}</td>
 										<td >{{ $reserva->user->name }}</td>
 
-                                            <td>                                                
+                                            <td>         @php echo auth()->user()->role; @endphp                                         
                                                 @if(auth()->user()->role === 'administrador')
                                                     {{-- Botón para administrador --}}
                                                     <a class="btn btn-sm btn-primary" href="{{ route('reservas.showReservaAdmin', $reserva->Localizador) }}">
