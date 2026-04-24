30d18ca03359ac874c8747c054d9276db3814ea4
alexvazu8 <alexvazu@gmail.com>
2026-04-24 10:37:35 -0400

    roles primera parte

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
 
 class User extends Authenticatable
@@ -44,6 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 
  
