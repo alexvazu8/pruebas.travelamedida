6040138a3e1dde0283304e2d4ff7739c4c4fe8ff
alexvazu8 <alexvazu@gmail.com>
2026-07-02 08:41:34 -0400

    placeholders de los campos de confirmacion

--- resources/views/carritos/show.blade.php
+++ resources/views/carritos/show.blade.php
@@ -354,19 +354,19 @@
                     <div class="row mb-3">
                         <div class="row md-6">
                             <label for="Nombre_titular_reserva">Nombre del Titular de la Reserva</label>
-                            <input type="text" name="Nombre_titular_reserva" value="{{ old('Nombre_titular_reserva') }}" id="Nombre_titular_reserva" class="form-control rounded-pill px-4"  maxlength="30"  pattern="[A-Za-zñÑ\s]+" oninput="this.value = this.value.replace(/[^A-Za-zñÑ\s]/g, '')" required>
+                            <input type="text" name="Nombre_titular_reserva" value="{{ old('Nombre_titular_reserva') }}" id="Nombre_titular_reserva" class="form-control rounded-pill px-4"  maxlength="30"  pattern="[A-Za-zñÑ\s]+" oninput="this.value = this.value.replace(/[^A-Za-zñÑ\s]/g, '')" placeholder="Titular que viaja" required>
                         </div>
                         <div class="row md-6">
                             <label for="Apellido_titular_reserva">Apellido del Titular de la Reserva</label>
-                            <input type="text" name="Apellido_titular_reserva" value="{{ old('Apellido_titular_reserva') }}" id="Apellido_titular_reserva" class="form-control rounded-pill px-4"  maxlength="30" pattern="[A-Za-zñÑ\s]+" oninput="this.value = this.value.replace(/[^A-Za-zñÑ\s]/g, '')" required>
+                            <input type="text" name="Apellido_titular_reserva" value="{{ old('Apellido_titular_reserva') }}" id="Apellido_titular_reserva" class="form-control rounded-pill px-4"  maxlength="30" pattern="[A-Za-zñÑ\s]+" oninput="this.value = this.value.replace(/[^A-Za-zñÑ\s]/g, '')" placeholder="Apellido del titular que viaja" required>
                         </div>
                         <div class="row md-6">
                             <label for="Telefono_titular_reserva">Telefono del Titular de la Reserva</label>
-                            <input type="text" name="Telefono_titular_reserva"  value="{{ old('Telefono_titular_reserva') }}" id="Telefono_titular_reserva" class="form-control rounded-pill px-4" pattern="^[+\d\s]+$" oninput="this.value = this.value.replace(/[^+\d\s]/g, '')" minlength="8" maxlength="20" required>
+                            <input type="text" name="Telefono_titular_reserva"  value="{{ old('Telefono_titular_reserva') }}" id="Telefono_titular_reserva" class="form-control rounded-pill px-4" pattern="^[+\d\s]+$" oninput="this.value = this.value.replace(/[^+\d\s]/g, '')" minlength="8" maxlength="20" placeholder="Ej: +59 12345678" required>
                         </div>
                         <div class="row mb-3">
                             <label for="Email_contacto_reserva">Email de Contacto</label>
-                            <input type="email" name="Email_contacto_reserva" value="{{ old('Email_contacto_reserva') }}" id="Email_contacto_reserva" class="form-control rounded-pill px-4" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Ejemplo: usuario@dominio.com"  required>
+                            <input type="email" name="Email_contacto_reserva" value="{{ old('Email_contacto_reserva') }}" id="Email_contacto_reserva" class="form-control rounded-pill px-4" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Ejemplo: usuario@dominio.com" placeholder="Ejemplo: usuario@dominio.com" required>
                         </div>
                          <div class="row mb-4">
                             <label for="Comentarios" class="col-md-6">RUC (Opcional)</label>
